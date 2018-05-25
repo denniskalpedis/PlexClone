c70077998ad2a8f14ccb1ffe955ee2e9350d5ba0
Liz <elizabethmpedley@gmail.com>
2018-05-25 10:41:25 -0500

    finished styling and hover effects

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -8,7 +8,7 @@ function toDom(arr){
         $('#moviesDisplay').html('<h1> No movies match your search</h1>');
     }
     for(var i=0;i<arr.length;i++){
-        $('#moviesDisplay').append('<div class="movieContainer col-3" onclick="renderMovie('+arr[i].id +')"><img src="'+arr[i].poster+'" alt=""><h3>'+arr[i].title+'</h3><p>'+arr[i].year+'</p></div>');
+        $('#moviesDisplay').append('<div class="movieContainer col-3 hover" onclick="renderMovie('+arr[i].id +')"><img src="'+arr[i].poster+'" alt=""><h3>'+arr[i].title+'</h3><p>'+arr[i].year+'</p></div>');
     }}
 
 function debounced(delay, fn) {
@@ -29,7 +29,7 @@ function renderMovie(movieId){
     var item = allItems.find(item => item.id == movieId );
     $('#moviesDisplay').hide();
     $('#singleMovieContainer').html('');
-    $('#singleMovieContainer').append('<p onclick="backLink()">Go Back</p><img src="'+item.poster+'" alt=""><h3>'+item.title+'<h3><p>'+item.year+'<p>');
+    $('#singleMovieContainer').append('<div class="left"><p class="hover goBack" onclick="backLink()">< Back</p><img src="'+item.poster+'" alt=""/><h3>'+item.title+'</h3><p>'+item.year+'</p></div><div class="right"><p><span>'+item.plot+'</span></p><p> Actors: <span>'+item.actors+'</span></p><p> Genre: <span>'+item.genre+'</span></p><p>File Runtime: <span>2:33</span><a href="/paly/'+item.id+'"><img class="play" src="Img/play.png"></a></p><p> Movie Length: <span>'+item.runtime+'</span></p><p>Movie Quality: <span>HD, 720p</span></p><p><img class="rotten"src="/Img/rotten-tomatoes.png" alt="rotten tomaotes icon."/> <span>'+item.rottenTomatoesRating+'</span></p><p><img class="rotten"src="/Img/imdb.png" alt="imdb icon."/> <span>'+item.imdbRating+'</span></p></div>');
     $('#singleMovieContainer').show();
 }
 
@@ -50,7 +50,7 @@ function renderLibrary(libraryId){
             $('#moviesDisplay').html('<h1> There are no Movies in this Library</h1>');
         }else{
             for(var i=0;i<library.length;i++){
-                $('#moviesDisplay').append('<div class="movieContainer col-3" onclick="renderMovie('+library[i].id +')"><img src="'+library[i].poster+'" alt=""><h3>'+library[i].title+'</h3><p>'+library[i].year+'</p></div>');
+                $('#moviesDisplay').append('<div class="movieContainer col-3 hover" onclick="renderMovie('+library[i].id +')"><img src="'+library[i].poster+'" alt=""><h3>'+library[i].title+'</h3><p>'+library[i].year+'</p></div>');
             }
         }
       });
@@ -66,6 +66,18 @@ $(document).ready(function(){
     $('#closeForm').on('click', function(){
         $('#formPopup').hide();
     });
+    $('.refresh').hide();
+    $('.delete').hide();
+
+    $(".library").hover(
+        function() {
+            $(this).children('.refresh').show();
+            $(this).children('.delete').show();
+        }, function() {
+            $(this).children('.refresh').hide();
+            $(this).children('.delete').hide();
+        }
+      );
 
     fetch('/JSON/allitems')
       .then(function(res) {
