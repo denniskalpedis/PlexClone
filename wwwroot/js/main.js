69c7369aac7cdadd8a3d55fffa13d17e0406d3ee
denniskalpedis <dennis.kalpedis@gmail.com>
2018-12-10 17:10:31 -0600

    reroute after video plays.

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -26,23 +26,37 @@ function debounced(delay, fn) {
     }
 var bounce = debounced(60, toDom);
 
-function renderMovie(movieId){
+function renderMovie(movieId, version = 0){
+    console.log(version);
+    localStorage.setItem("movie", movieId);
     var item = allItems.find(item => item.id == movieId );
     $('#moviesDisplay').hide();
     $('#singleMovieContainer').html('');
     var play;
     if(item.isplayable){
-        play = '<a href="/play/'+item.id+'"><img class="play" src="Img/play.png"></a>'
+        play = '<a href="/play/'+item.id+'"><img class="play" src="Img/play.png"></a>';
     }else{
-        play = ''
+        play = '';
     }
-    $('#singleMovieContainer').append('<div class="left"><p class="hover goBack" onclick="backLink()">< Back</p><img src="'+item.poster+'" alt=""/><h3>'+item.title+'</h3><p>'+item.year+'</p></div><div class="right"><p><span>'+item.plot+'</span></p><p> Actors: <span>'+item.actors+'</span></p><p> Genre: <span>'+item.genre+'</span></p><p>File Runtime: <span>'+ item.duration +'</span>'+play+'</p><p> Movie Length: <span>'+item.runtime+'</span></p><p>Movie Quality: <span>'+item.mulitple+'</span></p><p><img class="rotten"src="/Img/rotten-tomatoes.png" alt="rotten tomaotes icon."/> <span>'+item.rottentomatoesrating+'</span></p><p><img class="rotten"src="/Img/imdb.png" alt="imdb icon."/> <span>'+item.imdbrating+'</span></p></div>');
+    
+    // $('#singleMovieContainer').append('<dropdown class="dropleft float-right"><a class="hover goBack d-inline-block" href="#" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">...</a><dropdown class="dropdown-menu" aria-labelledby="dropdownMenu"></dropdown></dropdown><br>');
+    // $('.dropdown-menu').append('<a class="dropdown-item">Action</a><a class="dropdown-item">Another action</a><a class="dropdown-item">Something else here</a>');
+    $('#singleMovieContainer').append('<p class="hover goBack d-inline-block" onclick="backLink()">< Back</p><div class="dropleft float-right"><a class="hover goBack d-inline-block" href="#" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">...</a><dropdown class="dropdown-menu" aria-labelledby="dropdownMenu">' + getVersions(movieId) + '<a class="dropdown-item">Fix Match</a></dropdown></div><br><div class="left"><img src="'+item.poster+'" alt=""/><h3>'+item.title+'</h3><p>'+item.year+'</p></div><div class="right"><p><span>'+item.plot+'</span></p><p> Actors: <span>'+item.actors+'</span></p><p> Genre: <span>'+item.genre+'</span></p><p>File Runtime: <span>'+ item.duration +'</span>'+play+'</p><p> Movie Length: <span>'+item.runtime+'</span></p><p>Movie Quality: <span>'+item.mulitple+'</span></p><p><img class="rotten"src="/Img/rotten-tomatoes.png" alt="rotten tomaotes icon."/> <span>'+item.rottentomatoesrating+'</span></p><p><img class="rotten"src="/Img/imdb.png" alt="imdb icon."/> <span>'+item.imdbrating+'</span></p></div>');
     $('#singleMovieContainer').show();
+}
+
+function getVersions(){
+    return '<a class="dropdown-item">version</a>';
+}
+
+function qualityOfMovie(movieID){
+    return "<div>quality!!!!!</div>";
 }
 
 function backLink(){
     $('#singleMovieContainer').hide();
     $('#moviesDisplay').show();
+    localStorage.removeItem("movie");
 }
 
 function renderLibrary(libraryId){
@@ -100,8 +114,12 @@ deleteLibrary = (libraryId, event) => {
 
 
 
+
 $(document).ready(function(){
     $('#singleMovieContainer').hide();
+    $('#moviesDisplay').hide();
+    
+    
 
     $('#addLibraryButton').on('click', function(){
         $('#formPopup').show();
@@ -125,20 +143,25 @@ $(document).ready(function(){
     fetch('/JSON/allitems')
       .then(function(res) {
         return res.json();
-      }).then(data => allItems = data);
+      }).then(data => allItems = data).then(function(){
+        if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_BACK_FORWARD) {
+            if(localStorage.getItem("movie")){
+                renderMovie(localStorage.getItem("movie"));
+                localStorage.removeItem("movie");
+            } else {
+                $('#moviesDisplay').show();
+            }
+        } else {
+            $('#moviesDisplay').show();
+        }
+      });
 
+    
     $('#search').on('keyup', function(){
         clearTimeout(timeout);
         var string = $(this).val().toLowerCase();
         timeout = setTimeout(function(){
-            console.log(allItems.length);
-            for( let i = 0; i < allItems.length; i++){
-                console.log(allItems[i].title.toLowerCase().includes(string));
-                if(allItems[i].genre) {
-                    console.log(allItems[i].genre.toLowerCase().includes(string));
-                }
-            }
-
+            localStorage.removeItem("movie");
             var filtered = allItems.filter(item => ((item.title) ? item.title.toLowerCase().includes(string) : false) || ((item.genre) ? item.genre.toLowerCase().includes(string) : false));
             console.log(filtered);
             bounce(filtered);
